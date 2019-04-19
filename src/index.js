5006bd35a877b8947eaf23c64d7e9382a92f7728
everyonce <daniel@everyonce.com>
2019-04-19 07:08:18 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,60 +1,116 @@
 const setup = require('./starter-kit/setup');
+const dateFormat = require('dateformat');
 
-exports.handler = async (event, context, callback) => {
-  // For keeping the browser launch
-  context.callbackWaitsForEmptyEventLoop = false;
+// this handler signature requires AWS Lambda Nodejs v8.1
+exports.handler = async (event, context) => {
   const browser = await setup.getBrowser();
-  try {
-    const result = await exports.run(browser);
-    callback(null, result);
-  } catch (e) {
-    callback(e);
-  }
+
+  return await exports.run(browser);
 };
 
 exports.run = async (browser) => {
   // implement here
   // this is sample
-  const page = await browser.newPage();
-  await page.goto('https://www.google.co.jp',
-   {waitUntil: ['domcontentloaded', 'networkidle0']}
+  const page = (await browser.pages())[0];
+  await page.goto('https://www2.empoweredbymarathon.com/avs_marathon/jsp/login/marathon_login.jsp',
+      {waitUntil: ['domcontentloaded', 'networkidle0']}
   );
-  console.log((await page.content()).slice(0, 500));
 
-  await page.type('#lst-ib', 'aaaaa');
-  // avoid to timeout waitForNavigation() after click()
-  await Promise.all([
-    // avoid to
-    // 'Cannot find context with specified id undefined' for localStorage
-    page.waitForNavigation(),
-    page.click('[name=btnK]'),
-  ]);
+  // dom element selectors
+  const USERNAME_SELECTOR = 'input[name=username]';
+  const PASSWORD_SELECTOR = 'input[name=password]';
+  const BUTTON_SELECTOR = 'input[name=Submit]';
+  const CREDS = require('./marathoncreds');
 
-/* screenshot
-  await page.screenshot({path: '/tmp/screenshot.png'});
-  const aws = require('aws-sdk');
-  const s3 = new aws.S3({apiVersion: '2006-03-01'});
-  const fs = require('fs');
-  const screenshot = await new Promise((resolve, reject) => {
-    fs.readFile('/tmp/screenshot.png', (err, data) => {
-      if (err) return reject(err);
-      resolve(data);
+  // initial login
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDS.password);
+
+  await page.click(BUTTON_SELECTOR);
+  await page.waitForNavigation();
+
+  await page.goto('https://www2.empoweredbymarathon.com/avs_marathon/jsp/dailybusiness/bol.jsp');
+
+  const todayD=new Date();
+  const today=dateFormat(todayD, 'mm/dd/yyyy');
+  const yesterday=dateFormat(todayD-(60*1000*60*24), 'mm/dd/yyyy');
+  const filename='bol/marathon/'+dateFormat(todayD, 'yyyymmdd-HHMMss') +
+     '-marathon-bol.csv';
+  process.stdout.write('today: '+today+'\n');
+  process.stdout.write('yesterday: '+yesterday+'\n');
+
+  const START_DATE_SELECTOR = 'input[name=startDateOut]';
+  const END_DATE_SELECTOR = 'input[name=endDateOut]';
+  const SEARCH_BUTTON_SELECTOR = 'input[name=submit][type=image]';
+  // const FORM_SELECTOR = 'form[name=downloadbol]';
+  const DOWNLOAD_BUTTON_SELECTOR = 'input[name=button2][type=image]';
+
+
+  process.stdout.write('clicking start day selector\n');
+  const startInput = await page.$(START_DATE_SELECTOR);
+  await startInput.click({clickCount: 3});
+  process.stdout.write('typing: '+yesterday+'\n');
+  await startInput.type(yesterday);
+
+
+  process.stdout.write('clicking end day selector\n');
+  const endInput = await page.$(END_DATE_SELECTOR);
+  await endInput.click({clickCount: 3});
+  process.stdout.write('typing: '+today+'\n');
+  await endInput.type(today);
+
+  process.stdout.write('clicking search button selector\n');
+  await page.click(SEARCH_BUTTON_SELECTOR);
+  await page.waitForSelector(DOWNLOAD_BUTTON_SELECTOR);
+
+  await page.setRequestInterception(true);
+
+  page.click(DOWNLOAD_BUTTON_SELECTOR);
+
+  const xRequest = await new Promise((resolve) => {
+    page.on('request', (request) => {
+      process.stdout.write('intercepted request.\n');
+      request.abort();
+      resolve(request);
+      process.stdout.write('cancelled request.\n');
     });
   });
-  await s3.putObject({
-    Bucket: '<bucket name>',
-    Key: 'screenshot.png',
-    Body: screenshot,
-  }).promise();
-*/
 
-  // cookie and localStorage
-  await page.setCookie({name: 'name', value: 'cookieValue'});
-  console.log(await page.cookies());
-  console.log(await page.evaluate(() => {
-    localStorage.setItem('name', 'localStorageValue');
-    return localStorage.getItem('name');
-  }));
+
+  const request = require('request-promise');
+  const options = {
+    encoding: null,
+    method: xRequest._method,
+    uri: xRequest._url,
+    body: xRequest._postData,
+    headers: xRequest._headers,
+  };
+
+  /* add the cookies */
+  const cookies = await page.cookies();
+  options.headers.Cookie =
+       cookies.map((ck) => ck.name + '=' + ck.value).join(';');
+
+  process.stdout.write('sending new request.\n');
+  /* resend the request */
+  const response = await request(options);
+
+  // process.stdout.write(response);
+
+  const AWS = require('aws-sdk');
+
+  const s3 = new AWS.S3();
+  const params = {Bucket: 'rogers-bol-poc', Key: filename, Body: response};
+  s3.upload(params, function(err, data) {
+    console.log(err, data);
+  });
+  process.stdout.write('all done.\n');
+
   await page.close();
+
   return 'done';
 };
---
+++ src/marathoncreds.js
@@ -0,0 +1,4 @@
+module.exports = {
+  username: 'BCARR 01',
+  password: 'Mtown37814',
+};
